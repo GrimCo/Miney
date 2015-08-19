b0c3524765013e458c91fd4c85f849235d5aabde
Ace5852 <ace5852@fastmail.fm>
2015-08-18 22:36:51 -0400

    Bump Version To 1.0.0

--- src/main/java/com/ace5852/miney/Miney.java
+++ src/main/java/com/ace5852/miney/Miney.java
@@ -17,7 +17,7 @@ import net.minecraftforge.common.MinecraftForge;
 public class Miney
 {
     public static final String MODID = "miney";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1.0.0";
 
 
     //Config variables
