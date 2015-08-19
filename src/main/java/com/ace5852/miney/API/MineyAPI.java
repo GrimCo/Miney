1a365a83a164fa4e92f9bbb05ca1425b08083291
Ace5852 <ace5852@fastmail.fm>
2015-08-18 22:36:02 -0400

    Add JavaDoc Comments to API

--- src/main/java/com/ace5852/miney/API/MineyAPI.java
+++ src/main/java/com/ace5852/miney/API/MineyAPI.java
@@ -6,18 +6,35 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class MineyAPI
 {
+    /***
+     * Gets the balance of the player's bank account.
+     * @param player The player whose bank account you want.
+     * @return The balance of the bank account
+     */
     public double getBankBalance(EntityPlayer player)
     {
         DataHandler data = (DataHandler) player.getExtendedProperties("MineyDataHandler");
         return data.getHand();
     }
 
+    /***
+     * Gets the player's on hand balance.
+     * @param player The player whose on hand balance you want.
+     * @return The player's on hand balance.
+     */
     public double getMineyBalance(EntityPlayer player)
     {
         DataHandler data = (DataHandler) player.getExtendedProperties("MineyDataHandler");
         return data.getBank();
     }
 
+    /***
+     * Charge the Player. This is recommended for all transactions and comes out of the on hand balance.
+     * @param player The player to charge.
+     * @param amount The amount to charge/
+     * @return If the transaction was successful.
+     * Currently fails if there is not enough money to complete the transaction.
+     */
     public boolean chargePlayer(EntityPlayer player, double amount)
     {
         DataHandler data = (DataHandler) player.getExtendedProperties("MineyDataHandler");
@@ -32,6 +49,14 @@ public class MineyAPI
         return true;
     }
 
+    /***
+     * Charges the Player's bank account directly. This is recommended for use in ATMs and other things that should
+     * interact directly with the bank. It is not recommended to use for normal purchases.
+     * @param player The player whose bank account you wish to charge.
+     * @param amount The amount to charge.
+     * @return If the transaction was successful.
+     * Currently fails if there is not enough money to complete the transaction.
+     */
     public boolean chargePlayerBank(EntityPlayer player, double amount)
     {
         DataHandler data = (DataHandler) player.getExtendedProperties("MineyDataHandler");
